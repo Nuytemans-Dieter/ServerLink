e3853e6aaacffc10700e3ab66fa5f54b0f27b9d6
Nuytemans-Dieter <nuytemans_dieter@hotmail.com>
2020-09-16 21:23:49 +0200

    Finish profile screen v1

--- lib/user_interface/tab_screens/profile_screen.dart
+++ lib/user_interface/tab_screens/profile_screen.dart
@@ -8,8 +8,73 @@ class ProfileScreen extends StatefulWidget{
 }
 
 class _ProfileScreenState extends State<ProfileScreen>{
+
+  String displayname = 'Anonymous user';
+
+  TextEditingController _nameTextController = TextEditingController();
+
+  void saveName(String newName)
+  {
+    setState(() {
+      displayname = newName;
+    });
+  }
+
+  @override
+  void initState()
+  {
+    super.initState();
+
+    _nameTextController.text = displayname;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('This will be the profile screen');
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Profile settings',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          
+          Text('Displayname'),
+
+          // Name input field
+          TextFormField(
+            controller: _nameTextController,
+            onEditingComplete: () {
+              saveName( _nameTextController.text );
+            },
+          ),
+
+          // Save buttin
+          Align(
+            alignment: Alignment.centerRight,
+            child: IconButton(
+              tooltip: 'Save',
+              icon: Icon(
+                Icons.check,
+                color: Colors.green.shade400,
+              ), 
+              onPressed: () {
+                saveName( _nameTextController.text );
+              }
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
